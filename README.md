9193e10b3cca42c2f303c51edcc344a58083ea66
Tim Holtkötter <timholtkoetter.com@gmail.com>
2025-02-22 21:17:23 +0100

    Config creation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # linux-xmrig-quick-install
 
-Still WIP, not usable yet
+**WARNING** This repository is still WIP and currently supports *Ubuntu*
+
+This bash script provides an easy and fast setup for [XMRig](https://xmrig.com/).
+
+Quick way:
+```bash
+git clone https://github.com/TymHolt/linux-xmrig-quick-install.git && bash linux-xmrig-quick-install/installer.sh
+```
--- installer.sh
+++ installer.sh
@@ -7,29 +7,57 @@ if [[ !($confirm == [yY] || $confirm == [yY][eE][sS]) ]] then
 	exit 1
 fi
 
-# Step 1: Get and build repos according to https://xmrig.com/docs/miner/build/ubuntu
+# Step 1: Get and build repositories according to https://xmrig.com/docs/miner/build/ubuntu
 
 echo "Cloning repositories..."
-# sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-# git clone https://github.com/xmrig/xmrig.git
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
 
 read -p $'\n'"Do you want the advanced build? ((Y)es / (N)o): " confirm
 
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] then
 	echo "Advanced build..."
-	# mkdir xmrig/build && cd xmrig/scripts
-	# ./build_deps.sh && cd ../build
-	# cmake .. -DXMRIG_DEPS=scripts/deps
+	mkdir xmrig/build && cd xmrig/scripts
+	./build_deps.sh && cd ../build
+	cmake .. -DXMRIG_DEPS=scripts/deps
 else
 	echo " Basic build..."
-	# mkdir xmrig/build && cd xmrig/build
-	# cmake ..
+	mkdir xmrig/build && cd xmrig/build
+	cmake ..
 fi
 
-# make -j$(nproc)
+make -j$(nproc)
+
+cd ../../
 
 # Step 2: Install to the system
+# Copy binary and create config.json in /usr/local/bin
 
-# Create config.json in /usr/var?
+read -p $'\n'"Create new config? ((Y)es / (N)o): " confirm
+
+if [[ !($confirm == [yY] || $confirm == [yY][eE][sS]) ]] then
+	echo $'\n'"Finished"
+	exit 0
+fi
+
+# Create config file
+read -p "Mining pool URL: " poolurl
+read -p "Wallet address: " walletaddress
+
+echo "{" >> config.json
+echo $'\t'"\"autosave\": true," >> config.json
+echo $'\t'"\"cpu\": true," >> config.json
+echo $'\t'"\"opencl\": false," >> config.json
+echo $'\t'"\"cuda\": false," >> config.json
+echo $'\t'"\"pools\": [" >> config.json
+echo $'\t\t'"{" >> config.json
+echo $'\t\t\t'"\"url\": \"$poolurl\"," >> config.json
+echo $'\t\t\t'"\"user\": \"$walletaddress.worker0\"," >> config.json
+echo $'\t\t\t'"\"pass\": \"x\"," >> config.json
+echo $'\t\t\t'"\"keepalive\": true," >> config.json
+echo $'\t\t\t'"\"tls\": true" >> config.json
+echo $'\t\t'"}" >> config.json
+echo $'\t'"]" >> config.json
+echo "}" >> config.json
 
 echo $'\n'"Finished"
